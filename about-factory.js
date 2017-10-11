91cabd0dfeb7448ece65d80f1e1cd5c6f307841f
Kimberly Bird <kimberly.j.bird@gmail.com>
2017-10-11 15:50:02 -0500

    initial commit

---
+++ about-factory.js
@@ -0,0 +1,14 @@
+const history = {
+    "name": "History",
+    "text": "Looksie came about in October 2017 in Nashville, Tennessee. We are passionate about looking good while we code, which is what brought us to the world of eyewear."
+}
+
+const team = {
+    "name": "Our Team",
+    "text": {
+        "Alexis": "Alexis Hennecy: From Orlando, Florida",
+        "John": "John Dulaney: From Knoxville, Tennessee",
+        "Greg": "Greg Turner: Nashville Native",
+        "Kimmy": "Kimmy Bird: From Salt Lake City, Utah"
+    }
+}
