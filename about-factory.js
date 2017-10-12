5b34bbb059be5537abd8c6476aa709c270bcd8ee
Kimberly Bird <kimberly.j.bird@gmail.com>
2017-10-12 13:08:36 -0500

    about page push to github

--- about-factory.js
+++ about-factory.js
@@ -1,14 +1,23 @@
 const history = {
     "name": "History",
+    "aboutPic": "img/NSS.jpg",
     "text": "Looksie came about in October 2017 in Nashville, Tennessee. We are passionate about looking good while we code, which is what brought us to the world of eyewear."
 }
 
 const team = {
     "name": "Our Team",
-    "text": {
-        "Alexis": "Alexis Hennecy: From Orlando, Florida",
-        "John": "John Dulaney: From Knoxville, Tennessee",
-        "Greg": "Greg Turner: Nashville Native",
-        "Kimmy": "Kimmy Bird: From Salt Lake City, Utah"
-    }
+    "aboutPic": "img/Team-Photo.jpg",
+    "text":
+        [{"name": "Alexis Hennecy: From Orlando, Florida"},
+        {"name": "John Dulaney: From Knoxville, Tennessee"},
+        {"name": "Greg Turner: Nashville Native"},
+        {"name": "Kimmy Bird: From Salt Lake City, Utah"}]
 }
+
+let aboutLooksie = {
+    "history": history,
+    "team": team,
+}
+
+const aboutStringify = JSON.stringify(aboutLooksie)
+  localStorage.setItem("aboutLooksiePage", aboutStringify)
