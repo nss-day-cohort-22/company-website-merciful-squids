725b0b4b09b65a3ee2986fa98e1441c2ca0f9a8b
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2017-10-12 15:33:43 -0500

    added content

--- home-factory.js
+++ home-factory.js
@@ -6,16 +6,17 @@ const homeDatabase= {
 }
 
 const culture = {
-"text": "Here at Looksie we're a group of developers who have a passion to spread our fashion-forward specs on to you. 
+"text": "Here at Looksie we're a group of developers who have a passion to spread our fashion-forward specs on to you."
 
-"
 }
 
 
 
 
 const mission = {
+"text": "To increase our profit margins at a steady rate of 
 
+aka: to get rich off your poor eyesight"
 
 }
 
