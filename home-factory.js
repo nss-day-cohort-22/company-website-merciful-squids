5ab0549b86db074477a5c51b39a5b69fe83effe1
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2017-10-12 15:00:24 -0500

    saving new changes for home page

--- home-factory.js
+++ home-factory.js
@@ -2,8 +2,24 @@ const homeDatabase= {
 "contact": "contact.html",
 "about": "about.html",
 "products": "products.html",
-"hi": "http://www.warbyparker.com"
+
 }
+
+const culture = {
+"text": "Here at Looksie we're a group of developers who have a passion to spread our fashion-forward specs on to you. 
+
+"
+}
+
+
+
+
+const mission = {
+
+
+}
+
+
 
 const homeDatabaseString = JSON.stringify(homeDatabase)
 localStorage.setItem("homeDatabase", homeDatabaseString)
